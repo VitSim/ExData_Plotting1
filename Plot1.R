a7a53f300e4e9df25798a988d93c81c3ffc2ab5d
VitSim <vittoriosimeon@gmail.com>
2016-01-10 20:40:38 +0100

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,26 @@
+##STEP1: download, create the file, set date variables amd subset
+
+##set your Coursera Working Directory
+setwd("")
+# Download and unzip the dataset:
+fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileURL, destfile = "hpc.zip", method="curl")
+unzip("hpc.zip")
+# now import your file and create a table
+hpc<-read.table("household_power_consumption.txt", 
+                header = TRUE, sep = ";", stringsAsFactors = FALSE)
+# create a variable using strptime that represent Date and Time together
+hpc$datetime <- strptime(paste(hpc$Date, hpc$Time, sep=" "), 
+                         "%d/%m/%Y %H:%M:%S")
+# set the Date column as DATE to subset our dataset
+hpc$Date <- as.Date(hpc$Date, format="%d/%m/%Y")
+# do subset for two days
+subset<-subset(hpc, subset=(Date=="2007-02-01" | Date=="2007-02-02"))
+
+## STEP2: create all the figures
+
+#Plot 1
+hist(as.numeric(subset$Global_active_power), col="Red", 
+     xlab="Global Active Power (kilowatts)", main="Global Active Power")
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
